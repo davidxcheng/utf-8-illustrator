b334d3fed2e5827047b3a274290bc9310ccd4c98
David Cheng <davidxcheng@gmail.com>
2018-09-13 18:51:31 +0200

    Add legend for colors

--- docs/js/app.js
+++ docs/js/app.js
@@ -7,6 +7,7 @@ import octetsToHex from "./octetsToHex.js";
 var elInput = document.getElementById("txtInput");
 var elHeaders = document.getElementById("headers");
 var elOutput = document.getElementById("output");
+var elLegend = document.getElementById("legend");
 
 elInput.addEventListener("input", e => {
   elOutput.innerHTML = "";
@@ -14,9 +15,13 @@ elInput.addEventListener("input", e => {
   var text = e.target.value;
   window.location.hash = encodeURI(text);
 
-  text.length
-    ? elHeaders.classList.remove("hide")
-    : elHeaders.classList.add("hide");
+  if (text.length === 0) {
+    elHeaders.classList.add("hide");
+    elLegend.classList.add("hide");
+  } else {
+    elHeaders.classList.remove("hide");
+    elLegend.classList.remove("hide");
+  }
 
   for(var char of text) {
     let codePoint = char.codePointAt(0);
@@ -38,6 +43,7 @@ elInput.addEventListener("input", e => {
 window.addEventListener("load", () => {
   elInput.value = window.location.hash.length
     ? decodeURI(window.location.hash.slice(1))
+    // Default input (x, o, snowman, carrot)
     : decodeURI("x%C3%B8%E2%98%83%F0%9F%A5%95");
 
   // Trigger the input event
