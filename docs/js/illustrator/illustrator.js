97cb8a72466946cc3f826b94b8ef8963f0ad9723
David Cheng <davidxcheng@gmail.com>
2022-04-23 01:00:53 +0200

    Optimization: don't rerender entire output table when user is typing

---
+++ docs/js/illustrator/illustrator.js
@@ -0,0 +1,34 @@
+import codePointToUtf8 from "./codePointToUtf8.js";
+import octetsToMarkup from "./octetsToMarkup.js";
+import octetsToHex from "./octetsToHex.js";
+/**
+ * Creates markup for a table row (<tr>) that contains the char/glyph that is
+ * being illustrated (given that it's not a non-printable char) and its encoding.
+ * Slightly leaky that this component is aware that the UI presents the illustration
+ * in a table - but this is an application and not a lib:)
+ *
+ * @param char The character that is about to have its encoding illustrated.
+ *             The code point could have been used to resolve the char/glyph
+ *             but since the caller of this function already knows it we'll
+ *             take a shortcut
+ * @param codePoint The characters unicode codepoint (decimal)
+ * @returns A string that contains markup for the illustration of the char
+ *          and its encoding
+ */
+export default { createMarkup };
+function createMarkup(char, codePoint) {
+    const bin = codePoint.toString(2);
+    const hex = codePoint.toString(16);
+    const octets = codePointToUtf8(codePoint);
+    const utf8OctetsMarkup = octetsToMarkup(octets);
+    const utf8Hex = octetsToHex(octets);
+    return `
+    <tr>
+      <td class="glyph">${char}</td>
+      <td class="dec">${codePoint}</td>
+      <td class="hex"><a rel="nofollow noopener noreferrer" href="https://unicode-table.com/en/${hex}/">${hex}</a></td>
+      <td class="bin">${bin}</td>
+      <td>${utf8OctetsMarkup}</td>
+      <td class="hex">${utf8Hex}</td>
+    </tr>`;
+}
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,13 @@
-import codePointToUtf8 from "./codePointToUtf8.js";
-import octetsToMarkup from "./octetsToMarkup.js";
-import octetsToHex from "./octetsToHex.js";
+import illustrator from "./illustrator/illustrator.js";
+import input from "./input/input.js";
 
-const elForm = <HTMLElement>document.getElementById("frmInput");
-const elInput = <HTMLInputElement>document.getElementById("txtInput");
+const elHtml = document.documentElement;
 const elHeaders = <HTMLElement>document.getElementById("headers");
 const elOutput = <HTMLElement>document.getElementById("output");
 const elLegend = <HTMLElement>document.getElementById("legend");
 const elHintput = <HTMLElement>document.getElementById("hintput");
 
-const unicodeCodePointEscapeRegEx = /^\\u\{[A-Fa-f0-9]{1,6}\}$/;
-
-function createMarkup(char: string, codePoint: number): string {
-  const bin: string = codePoint.toString(2);
-  const hex: string = codePoint.toString(16);
-  const octets: string[] = codePointToUtf8(codePoint);
-  const utf8OctetsMarkup: string = octetsToMarkup(octets);
-  const utf8Hex: string = octetsToHex(octets);
-
-  return `<tr>
-      <td class="glyph">${char}</td>
-      <td class="dec">${codePoint}</td>
-      <td class="hex"><a rel="nofollow noopener noreferrer" href="https://unicode-table.com/en/${hex}/">${hex}</a></td>
-      <td class="bin">${bin}</td>
-      <td>${utf8OctetsMarkup}</td>
-      <td class="hex">${utf8Hex}</td>
-    </tr>`;
-}
-
-function render(text: string): void {
+function rerender(text: string): void {
   if (text.length === 0) {
     elHeaders.classList.add("hide");
     elLegend.classList.add("hide");
@@ -37,36 +16,60 @@ function render(text: string): void {
     elLegend.classList.remove("hide");
   }
 
-  if (unicodeCodePointEscapeRegEx.test(text)) {
-    // User input is Unicode code point escape (i.e. \u{FEFF}).
-    // Abort normal flow and just show that char
-    let hex = text.slice(3, -1);
-    let codePoint = parseInt(hex, 16);
-    elOutput.innerHTML = createMarkup(`&#x${hex}`, codePoint);
-
-    return;
-  }
-
   let ouputMarkup = "";
 
   for(const char of text) {
     const codePoint = <number>char.codePointAt(0);
-    ouputMarkup += createMarkup(char, codePoint);
+    ouputMarkup += illustrator.createMarkup(char, codePoint);
   }
 
   elOutput.innerHTML = ouputMarkup;
 }
 
-elInput.addEventListener("input", (e: Event) => {
-  const text = (<HTMLInputElement>e.target).value;
-  window.history.pushState({ text }, "Input", `#${text}`);
-  render(text);
-});
+function renderPushedInput(char: string) {
+  const template = document.createElement("template");
+  const codePoint = <number>char.codePointAt(0);
+  var markup = illustrator.createMarkup(char, codePoint);
 
-// Submitting the form causes a '?' to be added to the url
-elForm.addEventListener("submit", e => e.preventDefault());
+  template.innerHTML = markup;
+  elOutput.appendChild(template.content);
+}
+
+function renderFromHex(hex: string) {
+  const codePoint = parseInt(hex, 16);
+  elOutput.innerHTML = illustrator.createMarkup(`&#x${hex}`, codePoint);
+}
 
 window.addEventListener("load", () => {
+  const elForm = <HTMLFormElement>document.getElementById("frmInput");
+  const elInput = <HTMLInputElement>document.getElementById("txtInput");
+
+  input.setupUI(elForm, elInput);
+
+  elHtml.addEventListener(input.events.inputChanged, (e: CustomEvent) => {
+    const text = e.detail.input;
+    rerender(text);
+
+    if (e.detail.isPopStateInduced === false)
+      window.history.pushState({ text }, "Input", `#${text}`);
+  });
+
+  elHtml.addEventListener(input.events.inputPushed, (e: CustomEvent) => {
+    renderPushedInput(e.detail.pushedChar);
+    const text = e.detail.input;
+
+    if (e.detail.isPopStateInduced === false)
+      window.history.pushState({ text }, "Input", `#${text}`);
+  });
+
+  elHtml.addEventListener(input.events.hexInput, (e: CustomEvent) => {
+    renderFromHex(e.detail.hex);
+    const text = e.detail.input;
+
+    if (e.detail.isPopStateInduced === false)
+      window.history.pushState({ text }, "Input", `#${text}`);
+  });
+
   let text = "";
 
   if (window.location.hash.length) {
@@ -81,25 +84,16 @@ window.addEventListener("load", () => {
     text = decodeURI("x%C3%B8%E2%98%83%F0%9F%A5%95");
   }
 
-  window.history.pushState({ text }, "Input", `#${text}`);
-  elInput.value = text;
-  render(text);
+  input.set(text);
 });
 
 window.addEventListener("popstate", (e: PopStateEvent) => {
   if (e.state) {
-    elInput.value = e.state.text;
-    elInput.focus();
-    render(e.state.text);
+    input.set(e.state.text, true);
   }
 });
 
 elHintput.addEventListener("click", (e: Event) => {
   const escapedHexString = (<HTMLElement>e.target).innerText;
-
-  elInput.value = escapedHexString;
-  window.history.pushState({ text: escapedHexString }, "Input", `#${escapedHexString}`);
-  elInput.focus();
-
-  render(escapedHexString);
+  input.set(escapedHexString);
 });
