1c43adaca8b6fa25cbdd9fa1ae23a58d8c36ae3a
David Cheng <davidxcheng@gmail.com>
2022-09-24 16:58:12 +0200

    Perf: try to minimize rendering of table rows

--- docs/js/output/output.js
+++ docs/js/output/output.js
@@ -1,36 +1,50 @@
 import input from "../input/input.js";
 import createMarkup from "./illustrator/illustrator.js";
 import bitFlipper from "./bitFlipper.js";
+import outputEvents from "./customEvents.js";
 const elHtml = document.documentElement;
 const elHeaders = document.getElementById("headers");
 const elOutput = document.getElementById("output");
 const elLegend = document.getElementById("legend");
+let currentCodePoints = [];
 export default { setupUI };
 function setupUI() {
     elHtml.addEventListener(input.events.inputChanged, (e) => {
-        rerender(e.detail.codePoints);
+        render(e.detail.codePoints);
+    });
+    elHtml.addEventListener(outputEvents.bitFlipped, (e) => {
+        const { rowIndex: index, codePoint } = e.detail;
+        currentCodePoints[index] = codePoint;
     });
     bitFlipper.setupUI(elOutput);
 }
-function rerender(codePoints) {
+function render(codePoints) {
+    var _a;
     if (codePoints.length === 0) {
         elHeaders.classList.add("hide");
         elLegend.classList.add("hide");
+        elOutput.innerHTML = "";
+        currentCodePoints = [];
+        return;
     }
-    else {
-        elHeaders.classList.remove("hide");
-        elLegend.classList.remove("hide");
+    elHeaders.classList.remove("hide");
+    elLegend.classList.remove("hide");
+    for (let i = 0; i < codePoints.length; i++) {
+        if (i > currentCodePoints.length - 1) {
+            appendOutput(codePoints[i]);
+            continue;
+        }
+        if (codePoints[i] !== currentCodePoints[i]) {
+            elOutput.children[i].innerHTML = createMarkup(codePoints[i]);
+        }
     }
-    let ouputMarkup = "";
-    for (const codePoint of codePoints) {
-        ouputMarkup += createMarkup(codePoint);
+    for (let i = codePoints.length; i < currentCodePoints.length; i++) {
+        (_a = elOutput.lastChild) === null || _a === void 0 ? void 0 : _a.remove();
     }
-    elOutput.innerHTML = ouputMarkup;
+    currentCodePoints = codePoints;
 }
-function renderPushedInput(char) {
+function appendOutput(codePoint) {
     const template = document.createElement("template");
-    const codePoint = char.codePointAt(0);
     template.innerHTML = createMarkup(codePoint);
-    ;
     elOutput.appendChild(template.content);
 }
--- src/output/output.ts
+++ src/output/output.ts
@@ -1,6 +1,7 @@
 import input from "../input/input.js";
 import createMarkup from "./illustrator/illustrator.js";
 import bitFlipper from "./bitFlipper.js";
+import outputEvents from "./customEvents.js";
 
 const elHtml: HTMLElement = document.documentElement;
 
@@ -8,38 +9,58 @@ const elHeaders = <HTMLElement>document.getElementById("headers");
 const elOutput = <HTMLElement>document.getElementById("output");
 const elLegend = <HTMLElement>document.getElementById("legend");
 
+let currentCodePoints: number[] = [];
+
 export default { setupUI };
 
 function setupUI() {
   elHtml.addEventListener(input.events.inputChanged, (e: CustomEvent) => {
-    rerender(<number[]>e.detail.codePoints);
+    render(<number[]>e.detail.codePoints);
+  });
+
+  elHtml.addEventListener(outputEvents.bitFlipped, (e: CustomEvent) => {
+    const { rowIndex: index, codePoint } = e.detail;
+    currentCodePoints[index] = codePoint;
   });
 
   bitFlipper.setupUI(elOutput);
 }
 
-function rerender(codePoints: number[]): void {
+function render(codePoints: number[]): void {
   if (codePoints.length === 0) {
     elHeaders.classList.add("hide");
-    elLegend.classList.add("hide")
-  } else {
-    elHeaders.classList.remove("hide");
-    elLegend.classList.remove("hide");
+    elLegend.classList.add("hide");
+    elOutput.innerHTML = "";
+    currentCodePoints = [];
+    return;
   }
 
-  let ouputMarkup = "";
+  elHeaders.classList.remove("hide");
+  elLegend.classList.remove("hide");
 
-  for(const codePoint of codePoints) {
-    ouputMarkup += createMarkup(codePoint);
+  for(let i = 0;i < codePoints.length; i++) {
+    if (i > currentCodePoints.length - 1) {
+      appendOutput(codePoints[i]);
+      continue;
+    }
+
+    // Prevent re-rendering of rows that hasn't changed (user is typing?)
+    if (codePoints[i] !== currentCodePoints[i]) {
+      elOutput.children[i].innerHTML = createMarkup(codePoints[i]);
+    }
   }
 
-  elOutput.innerHTML = ouputMarkup;
+  // Remove rows at the end if incoming text is shorter than current
+  for(let i = codePoints.length; i < currentCodePoints.length; i++) {
+    elOutput.lastChild?.remove();
+  }
+
+  currentCodePoints = codePoints;
 }
 
-function renderPushedInput(char: string) {
+function appendOutput(codePoint: number) {
   const template = document.createElement("template");
-  const codePoint = <number>char.codePointAt(0);
+  template.innerHTML = createMarkup(codePoint);
 
-  template.innerHTML = createMarkup(codePoint);;
   elOutput.appendChild(template.content);
 }
